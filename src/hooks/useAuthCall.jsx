3250b58d8dff3592f522b05c1e3e6740bb6d2a1c
hasan <hasanakoz1986@gmail.com>
2022-11-15 22:40:51 +0300

    custom hook done

---
+++ src/hooks/useAuthCall.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import axios from "axios";
+import { fetchFail, fetchStart, loginSuccess } from "../features/authSlice";
+import { useDispatch } from "react-redux";
+
+const BASE_URL = "https://clarusway.pythonanywhere.com/";
+
+const useAuthCall = () => {
+  const dispatch = useDispatch();
+  const login = async (userInfo) => {
+    try {
+      dispatch(fetchStart());
+      const { data } = await axios.post(
+        `${BASE_URL}account/auth/login/`,
+        userInfo
+      );
+      dispatch(loginSuccess());
+      return data;
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+    }
+  };
+
+  return { login };
+};
+
+export default useAuthCall;
